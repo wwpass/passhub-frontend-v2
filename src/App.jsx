80cb973d3e543faca629d6affa0fe4736b79b745
Mikhail Vysogorets <m.vysogorets@wwpass.com>
2024-04-28 23:24:28 -0400

    code cleanup

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,8 @@
-import { useState, useEffect } from 'react'
-
 import './style.scss'
 
 import Root from './components/root';
 
 function App() {
-
-  /*  
-  
-     console.log('rendering App');
-  
-    useEffect(() => {
-      console.log('App mounted');
-      return () => {
-          console.log('App unmounted');
-      };
-    }, []);
-  */
-
   return (
     <Root key='root'></Root>
   )
