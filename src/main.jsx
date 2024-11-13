2242c99948affb6870037ccfca8613041a87e351
Mikhail Vysogorets <m.vysogorets@wwpass.com>
2024-11-13 18:05:55 -0500

    trap for errors ErrorBoundary

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,33 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { QueryClient, QueryClientProvider, focusManager } from '@tanstack/react-query';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import App from './App'
 // import './style.css'
 // import './index.css'
 
+import { ErrorBoundary } from "react-error-boundary";
 
 const queryClient = new QueryClient();
 
+
+
+function fallbackRender({ error, resetErrorBoundary }) {
+  // Call resetErrorBoundary() to reset the error boundary and retry the render.
+
+  return (
+    <div role="alert">
+      <p>Something went wrong:</p>
+      <pre>{error.message}</pre>
+    </div>
+  );
+}
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <QueryClientProvider client={queryClient}>
-    <App />
-  </QueryClientProvider>
+  <ErrorBoundary fallbackRender={fallbackRender}>
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
+  </ErrorBoundary>
 )
